8635dd7d798393b98602c22a6deacca948cee3a0
Fahmy Sharaf <fahmysharf@gmail.com>
2021-01-21 21:00:09 +0200

    cron worker to fetch data and filtering algorithem + redis  + react

---
+++ README.md
@@ -0,0 +1,7 @@
+Stack / things built in this repo.
+
+= React front end w/ Hooks
+= Simple Node-Express API
+= Cron worker to fetch data
+= Simple filtering algorithm
+= Redis + node-redis
---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import Jobs from "./components/Jobs";
+import fetch from "node-fetch";
+const JOB_API_URL = "/api/jobs";
+
+async function fetchJobs(updateCb) {
+  const res = await fetch(JOB_API_URL);
+  let json = await res.json();
+
+  updateCb(json);
+}
+
+function App() {
+  const [jobList, updateJobs] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchJobs(updateJobs);
+  }, []);
+
+  return (
+    <div className="App">
+      <Jobs jobs={jobList} />
+    </div>
+  );
+}
+
+export default App;
